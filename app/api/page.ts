7d320f4d9f35f598d87e56e75b1edf7d8b899fbd
KeneEzeh <ezeanwe.kenechukwu@decagon.dev>
2024-02-28 16:53:23 +0100

    deployed to vercel

--- app/api/page.ts
+++ app/api/page.ts
@@ -1,18 +1,18 @@
-// pages/api/nearbyRestaurants.ts
-import { NextApiRequest, NextApiResponse } from 'next';
-import axios from 'axios';
+// // pages/api/nearbyRestaurants.ts
+// import { NextApiRequest, NextApiResponse } from 'next';
+// import axios from 'axios';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { location } = req.query;
+// export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+//   const { location } = req.query;
 
-  try {
-    const response = await axios.get(
-      `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${location}&radius=500&type=restaurant&key=AIzaSyCIkwbPl27UP-aWNFqnmzSiZujGPsJUhnM`
-    );
+//   try {
+//     const response = await axios.get(
+//       `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${location}&radius=500&type=restaurant&key=AIzaSyCIkwbPl27UP-aWNFqnmzSiZujGPsJUhnM`
+//     );
 
-    res.status(200).json(response.data.results);
-  } catch (error) {
-    console.error('Error fetching nearby restaurants:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-}
+//     res.status(200).json(response.data.results);
+//   } catch (error) {
+//     console.error('Error fetching nearby restaurants:', error);
+//     res.status(500).json({ error: 'Internal Server Error' });
+//   }
+// }
